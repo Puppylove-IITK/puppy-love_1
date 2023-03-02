d37b8411b428be0cbb2b30825dfdec6754cf03c6
Akhilstaar <iakhilmeena@gmail.com>
2023-03-02 15:05:29 +0530

    Main

--- Backend/db/mongo.go
+++ Backend/db/mongo.go
@@ -3,7 +3,6 @@ package db
 import (
 	"github.com/Puppylove-IITK/puppylove/config"
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 type PuppyDb struct {
@@ -11,23 +10,14 @@ type PuppyDb struct {
 }
 
 func MongoConnect() (PuppyDb, error) {
-	// Create MongoDB DialInfo object using MongoDB URI with authentication credentials
-	mongoInfo := &mgo.DialInfo{
-		Addrs:    []string{config.CfgMgoUrl},
-		Database: config.CfgMgoDb,
-		Username: config.CfgMgoUser,
-		Password: config.CfgMgoPass,
-	}
-
-	// Create a new session using DialWithInfo
-	S, err := mgo.DialWithInfo(mongoInfo)
+	S, err := mgo.Dial(config.CfgMgoUrl)
 	return PuppyDb{S}, err
 }
 
 func (db PuppyDb) GetById(table string, id string) *mgo.Query {
-	return db.S.DB(config.CfgMgoDb).C(table).FindId(bson.ObjectIdHex(id))
+	return db.S.DB("puppy").C(table).FindId(id)
 }
 
 func (db PuppyDb) GetCollection(table string) *mgo.Collection {
-	return db.S.DB(config.CfgMgoDb).C(table)
+	return db.S.DB("puppy").C(table)
 }
